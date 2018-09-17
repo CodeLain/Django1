7e751737507631a441f642c28104559938f27793
Federico <federico.alberti.gurnerio@gmail.com>
2018-09-17 16:40:00 -0300

    initial commit

---
+++ TestProject/CinemaCore/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+class MyView(View):
+    def get(self, request):
+        print('something')
+        return HttpResponse('result')
+
